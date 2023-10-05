b6598669bb363a7bfdb81b2d2dfc618cb14f3d94
VanVAPhil <147021460+VanVAPhil@users.noreply.github.com>
2023-10-05 23:16:19 +0800

    Update boardinghouses.java

--- boardinghouses.java
+++ boardinghouses.java
@@ -1,37 +1,52 @@
-package kunganolang;
-
 public class boardinghouses {
 		
-		int arrnum = 3;
-		String BUCampuses[] = {"BU Tabaco", "BU Gubat", "BU Main", "BU Polangui"};
+		int arrnum = 5;
+		String BUCampuses[] = {"BU MAIN","BU DARAGA","BU GUINOBATAN","BU GUBAT","BU POLANGUI","BU TABACO"};
+		String lowerBUCampuses[] = {"main", "DARAGA", "GUINOBATAN", "GUBAT","polangui","tabaco"};
+		String lowerCBUCampuses[] = {"bu main", "bu daraga", "bu guinobatan", "bu gubat", "bu polangui", "bu tabaco"};
 		String BHouses[][] = {
-				{"Tabaco1", "T2 Diks", "T3"},
-				{"Gf Day", "GUBAT", "GO Here"}, 
-				{"Malaki", "Main BH", "Ito pa"},
-				{"Estiller", "Immaculate", "Bahay ni Jaycent"}
+				{"Cozy Haven Boarding", "Serene Stay Residences", "Tranquil Retreat Inn", "Harmony House", "Sunset Sands Boarding"},
+				{"Riverside Rest", "Hilltop Hideaway", "Seaside Breeze Lodge", "Whispering Pines Inn", "Urban Hotel"},
+				{"Kanor Living", "Filo House", "BHAUSING", "Booth House", "Felixia"},
+				{"Moonlight Mansion", "Victorian Charm Inn", "Coastal Comfort Lodge", "Starry Night Haven", "Mang Tomas BH"},
+				{"Styler’s Dormitory", "Estiller", "Emmaculate", "Aurora Manor", "Meadow Lane Lodge"},
+				{"Skylight Suites", "Bayside Bliss Boarding", "Golden Gate Lodging", "Forest Edge", "Cherry Blossom Boarding"}
 				};
 		String[][] BHDeets = {
-				{"Malinis", "Mura Lang", "Medyo Madumi"},
-				{"Kita sa labas CR", "Madami CHiks", "..."},
-				{"Walang Bayad", "Libre Pagkain", "Libre Lahat"},
-				{"Mixed kaya may chiks", "3rd floor start", "White house"}
+				{"Delightful","Mesmerizing","Alluring","Exquisite","Captivating"},
+				{"Charming","Picturesque","Breathtaking","Enchanting","Lively"},
+				{"Clean","Aesthetic","Brisk","Pure","Stunning"},
+				{"Elegant","Cozy","Tranquil","Well-maintained","Student-friendly"},
+				{"Welcoming","Spacious","Comfortable","Convenient","Organized"},
+				{"Secure","Hospitable","Affordable","Budget-friendly","Peaceful"}
 				};
 		String BHPeople[][] = {
-				{"1", "2", "10"},
-				{"1000", "30", "10"},
-				{"0", "9", "69"},
-				{"50", "70", "1"}
+				{"7","5","4","2","1"},
+				{"3","5","2","1","7"},
+				{"5","8","3","3","2"},
+				{"4","3","3","2","9"},
+				{"4","5","4","5","1"},
+				{"1","3","2","2","7"}
 				};
-		String BHPrice[][] = {
-				{"3000", "4000", "3500"},
-				{"2000", "3000", "2500"},
-				{"0", "9000", "0"},
-				{"1500", "1500", "300"}
+		String BHPrice[][] = { //IN PHP
+				{"600","1000","1100","3000","4000"},
+				{"1250","1050","2000","4500","950"},
+				{"1000","800","2500","2100","3000"},
+				{"1100","1500","2000","3000","900"},
+				{"1100","1200","1500","1000","5000"},
+				{"6000","1500","2000","3000","850"}
 				};
-		String BHDistance[][] = { //In meters
-				{"500", "2000", "300"},
-				{"100", "878", "6969"},
-				{"10", "8999", "1"},
-				{"50", "54", "300"}
+		String BHDistance[][] = { //In METERS
+				{"10","15","20","25","40"},
+				{"9","30","15","5","20"},
+				{"50","10","30","25","12"},
+				{"5","15","60","11","25"},
+				{"30","15","20","50","25"},
+				{"50","10","15","30","60"}
 				};
+		String BHRanges [][]= {
+				{"1-2","3-4","5-6","7 or more"},
+				{"500-1000","1001-2000","2001-3000","3001-4000","4001-more"},
+				{"0-9","10-19","20-29","30-39","40 or more"}
+		};
 }
